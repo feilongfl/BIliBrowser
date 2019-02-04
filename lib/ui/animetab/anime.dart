94660941d31eb783b4b694c40d49824fb6803da4
bilibrowser <bilibrowser@bilibili.com>
2019-02-04 11:37:36 +0800

    init

---
+++ lib/ui/animetab/anime.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//Widget AnimeTab = Center(
+//  child: Icon(Icons.texture),
+//);
+
+class AnimeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: Icon(Icons.play_circle_filled),
+    );
+  }
+}
